e0f85bc14f44d4c04616e7d1353f8dc10792e116
Alex McClead <n7q4c1@u.northwestern.edu>
2019-10-01 14:54:47 -0500

    Add Main Files

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,2 +1,72 @@
-public class Encrypt {
+public interface Encrypt {
+    String encrypt(String message, int key);
+    String decrypt(String message, int key);
+}
+
+class MoveLetterAlgEncrypt implements Encrypt {
+
+    final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public String encrypt(String message, int key){
+
+        String result = "";
+        char[] messageArray = message.toLowerCase().toCharArray();
+
+        for (char letter : messageArray){
+
+            if (ALPHABET.indexOf(letter)>=0) {
+
+                int move = ((ALPHABET.indexOf(letter) + key ) % ALPHABET.length());
+
+                if (move < 0) {
+                    move = (ALPHABET.length()-1)+move;
+
+                }
+                result += ALPHABET.charAt(move);
+            }
+            else {
+                result += letter;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String decrypt(String message, int key){
+        String result = "";
+        char[] messageChar =  message.toLowerCase().toCharArray();
+        for (char letter : messageChar){
+            int pos = ALPHABET.indexOf(letter);
+            int move = (pos - key) % ALPHABET.length();
+            if (move<0){
+                move = ALPHABET.length()+move;
+            }
+            result+= (ALPHABET.charAt(move));
+        }
+        return result;
+
+    }
+}
+
+class BitShiftAlgEncrypt implements Encrypt{
+
+    public String encrypt(String message, int key){
+
+        String response = "";
+
+        char[] messageArray = message.toCharArray();
+
+        for (char letter : messageArray){
+            letter += key;
+
+            response += letter;
+        }
+
+        return response;
+    }
+
+    @Override
+    public String decrypt(String message, int key){
+        return encrypt(message, key*-1);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,85 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        String mode = "enc";
+        String data = null;
+        String inFilename = null;
+        String outFilename = null;
+        String algorithm = "shift";
+        String response = "";
+        Encrypt encryptAlg = null;
+
+        int key = -1;
+
+        for (int i = 0, j = 1; j < args.length; i += 2, j += 2) {
+            String arg = args[i];
+            String option = args[j];
+
+
+            if ("-in".equals(arg)) {
+                inFilename = option;
+            }
+            if ("-out".equals(arg)) {
+                outFilename = option;
+            }
+            if ("-mode".equals(arg)) {
+                mode = option;
+            }
+            if ("-data".equals(arg)) {
+                data = option;
+            }
+            if ("-key".equals(arg)) {
+                key = Integer.parseInt(option);
+            }
+            if ("-alg".equals(arg)) {
+                algorithm = option;
+            }
+        }
+
+        if (inFilename != null) {
+            data = "";
+            Scanner fileScan = new Scanner(new File(inFilename));
+            while (fileScan.hasNextLine()) {
+                data += fileScan.nextLine();
+            }
+            fileScan.close();
+        }
+
+        if (data == null) {
+            data = scan.nextLine();
+        }
+
+        if (key == -1) {
+            key = scan.nextInt();
+        }
+
+        if ("shift".equals(algorithm)) {
+            encryptAlg = new MoveLetterAlgEncrypt();
+        }
+
+        if ("unicode".equals(algorithm)) {
+            encryptAlg = new BitShiftAlgEncrypt();
+        }
+
+        if ("enc".equals(mode)) {
+            response = encryptAlg.encrypt(data, key);
+        } else if (mode.equals("dec")) {
+            response = encryptAlg.decrypt(data, (key));
+        }
+        if (outFilename == null) {
+            System.out.println(response);
+        } else {
+            PrintWriter writer = new PrintWriter(new File(outFilename));
+            writer.println(response);
+            writer.close();
+        }
+
+    }
 }
